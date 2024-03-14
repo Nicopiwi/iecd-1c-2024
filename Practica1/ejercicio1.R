d1ad50caf682905b39e1ee5c65b09d5bf6f60589
Nicopiwi <nikorozenberg@gmail.com>
2024-03-14 20:38:47 -0300

    indentation

--- Practica1/ejercicio1.R
+++ Practica1/ejercicio1.R
@@ -5,7 +5,7 @@ data_patients$diagnosis <- as.character(data_patients$diagnosis)
 get_relative_freqs_by_diagnosis <- function(diagnosis) {
   unique_groups <- unique(diagnosis)
   freqs <- numeric(length(unique_groups))
-  for (i in 1:length(unique_groups)) {
+  for (i in seq_along(unique_groups)) {
     freqs[i] <- mean(diagnosis == unique_groups[i])
   }
   data.frame(diagnosis = unique_groups, relative_freq = freqs)
@@ -16,10 +16,10 @@ relative_freqs_by_diagnosis <- get_relative_freqs_by_diagnosis(
 )
 
 barplot(
-  relative_freqs_by_diagnosis$relative_freq, 
-  names.arg = relative_freqs_by_diagnosis$diagnosis, 
-  xlab = "Diagnóstico", 
-  ylab = "FR", 
+  relative_freqs_by_diagnosis$relative_freq,
+  names.arg = relative_freqs_by_diagnosis$diagnosis,
+  xlab = "Diagnóstico",
+  ylab = "FR",
   main = "Frecuencias relativas por diagnóstico",
-  ylim=c(0, 1)
+  ylim = c(0, 1)
 )
--- Practica1/ejercicio2.R
+++ Practica1/ejercicio2.R
@@ -1,45 +1,45 @@
 data_titanic <- read.csv("./data/datos_titanic.csv")
 
-get_proportion_woman_from_survived <- function(){
-    # Por default, el índice de un DataFrame es en el eje de columnas
-    survived <- data_titanic[data_titanic$Survived == 1, ]
-    
-    return(sum(survived$Sex == "female")/nrow(survived))
+get_proportion_woman_from_survived <- function() {
+  # Por default, el índice de un DataFrame es en el eje de columnas
+  survived <- data_titanic[data_titanic$Survived == 1, ]
+
+  return(sum(survived$Sex == "female") / nrow(survived))
 }
 
-create_contingency_table_pclass_survived <- function(){
-    contingency_table <- table(
-        data_titanic$Survived, 
-        data_titanic$Pclass
-    )
-    
-    return(contingency_table)
+create_contingency_table_pclass_survived <- function() {
+  contingency_table <- table(
+    data_titanic$Survived,
+    data_titanic$Pclass
+  )
+
+  return(contingency_table)
 }
 
 print(sprintf(
-    "Proporción de mujeres dentro de sobrevivientes: %f",
-    get_proportion_woman_from_survived()
+  "Proporción de mujeres dentro de sobrevivientes: %f",
+  get_proportion_woman_from_survived()
 ))
 
 contingency_table <- create_contingency_table_pclass_survived()
 
 estimated_probs <- apply(
-    contingency_table, 2, 
-    function (pclass) pclass[1] / sum(pclass)
+  contingency_table, 2,
+  function(pclass) pclass[1] / sum(pclass)
 )
 
-for (pclass in colnames(contingency_table)){
-    print(sprintf(
-        "Probabilidad estimada de sobrevivir para clase %s es %f", 
-        pclass, 
-        estimated_probs[pclass]
-    ))
+for (pclass in colnames(contingency_table)) {
+  print(sprintf(
+    "Probabilidad estimada de sobrevivir para clase %s es %f",
+    pclass,
+    estimated_probs[pclass]
+  ))
 }
 
 barplot(
-    contingency_table, 
-    legend = c("No", "Sí"),
-    main="Grafico de contingencia",
-    xlab="Clase de pasajero", 
-    ylab="Sobrevivió",
+  contingency_table,
+  legend = c("No", "Sí"),
+  main = "Grafico de contingencia",
+  xlab = "Clase de pasajero",
+  ylab = "Sobrevivió",
 )
